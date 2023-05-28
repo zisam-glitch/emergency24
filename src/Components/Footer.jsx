58d78a95d2706b80d219d0ee77f40c0775e975a4
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-05-28 21:51:48 +0600

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../Images/white.png";
+import Grow from "../Images/grow.jpg";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -13,18 +14,23 @@ function Footer() {
           <div className="logoz">
             <img src={Logo} alt="" />
           </div>
+          <div className="list">
           <div className="iconm">
             <FaFacebookSquare />
             <FaTwitter />
             <FaInstagram />
             <FaGoogle />
           </div>
+         
+          </div>
         </div>
-        </div>
-        <div className="coppy">
-          <hr />
-          <h5>Copyright 2023 emergency247dentist</h5>
-        </div>
+        
+      </div>
+
+      <div className="coppy">
+        <hr />
+        <h5>Copyright 2023 emergency247dentist | Website <a href="https://instagram.com/grow_with_sayed?igshid=MzRlODBiNWFlZA=="> Build By Sayed</a></h5>
+      </div>
     </section>
   );
 }
